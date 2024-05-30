aa39f41e9267388af86b51e74a26af2f7ab346b6
abanob.anis <abanob.anis@fateel.co>
2024-05-30 21:09:37 +0300

    capsule_movement

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NewBehaviourScript : MonoBehaviourPunCallbacks
+{
+
+    public GameObject player;
+    [Space]
+    public Transform spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting..");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("Connected to Server!");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("test" ,null ,null);
+
+        Debug.Log("Connected to Room");
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        GameObject _player = PhotonNetwork.Instantiate(player.name ,spawnPoint.position ,Quaternion.identity);
+
+    }
+}
